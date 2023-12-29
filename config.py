fbbf8294933f0635ca11a3efd971fd5bc36ddda7
nikolay <Nikc-mk@mail.ru>
2023-12-29 11:22:52 +1000

    Подготовить alembic к миграции, подготовить постгресс к миграции, сделать апгрейд бд

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PG_HOST = os.environ.get("PG_HOST")
+PG_PORT = os.environ.get("PG_PORT")
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+POSTGRES_DB = os.environ.get("POSTGRES_DB")
